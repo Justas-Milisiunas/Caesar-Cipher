a7b9d8f8d1eb41852950e818074936ffbcf79e9f
Justas-Milisiunas <justas.milisiunas@gmail.com>
2020-10-31 10:11:05 +0200

    Implemented caesar cipher algorithm

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class CaesarCipher : ICaesarCipher
+    {
+        public string Encrypt(string plainText, int shift)
+        {
+            return CaesarCipherAlhorithm(plainText, shift);
+        }
+
+        public string Decrypt(string cipherText, int shift)
+        {
+            return CaesarCipherAlhorithm(cipherText, -shift);
+        }
+
+        private static string CaesarCipherAlhorithm(string text, int shift)
+        {
+            char[] letters = text.ToCharArray();
+            StringBuilder modifiedText = new StringBuilder();
+
+            foreach (var letter in letters)
+            {
+                char shiftedLetter = ShiftLetter(letter, shift);
+                modifiedText.Append(shiftedLetter);
+            }
+
+            return modifiedText.ToString();
+        }
+
+        private static char ShiftLetter(char letter, int shift)
+        {
+            return (char)(letter + shift);
+        }
+    }
+}
---
+++ CaesarCipher/ICaesarCipher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipher
+{
+    interface ICaesarCipher
+    {
+        string Encrypt(string plainText, int shift);
+        string Decrypt(string cipherText, int shift);
+    }
+}
